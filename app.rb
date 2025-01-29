512abfaf647e5480899f98f29aafb47fda475476
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-01-29 00:19:43 +0000

    refactored

--- app.rb
+++ app.rb
@@ -5,54 +5,6 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/2/6") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..6)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_six)
-end
-
-get("/dice/2/10") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..10)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_ten)
-end
-
-get("/dice/1/20") do
-  @rolls = []
-
-  1.times do
-    die = rand(1..20)
-
-    @rolls.push(die)
-  end
-
-  erb(:one_twenty)
-end
-
-get("/dice/5/4") do
-  @rolls = []
-
-  5.times do
-    die = rand(1..4)
-
-    @rolls.push(die)
-  end
-
-  erb(:five_four)
-end
-
 get("/dice/:number_of_dice/:number_of_sides") do
   @rolls = []
   @num_dice = params.fetch("number_of_dice").to_i
