a088171d6bc78d6fd7ff42193338f11d888592d8
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-01-29 00:14:50 +0000

    fully dynamic

--- app.rb
+++ app.rb
@@ -52,3 +52,16 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+
+get("/dynamic/:number_of_dice/:number_of_sides") do
+  @rolls = []
+  @num_dice = params.fetch("number_of_dice").to_i
+  @sides = params.fetch("number_of_sides").to_i
+
+  @num_dice.times do
+    die = rand(1..@sides)
+    @rolls.append(die)
+  end
+
+  erb(:flexible)
+end
